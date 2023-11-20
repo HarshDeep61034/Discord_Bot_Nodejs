484aa41d57f9742b98bc4b6dcd2b99a31976f85b
Harsh Deep <hdeep61034@gmail.com>
2023-11-20 05:32:41 +0530

    Intialized Discord Bot Project

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+import dotenv from 'dotenv';
+dotenv.config();
+const token = process.env.DISCORD_BOT_TOKEN;
+
+client.on("messageCreate", (message) => {
+	message.reply("Hello Chutiye");
+	console.log(message);
+})
+
+client.login(token);
